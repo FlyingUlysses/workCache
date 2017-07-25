e23b912005621e96f096c60ac36c30b2cb3995a9
flyingulysses <wangyong_coder@qq.com>
2017-07-25 09:39:43 +0800

    sql_base

---
+++ odorsManager/sql/dml_Dict_ODORS_METHOD.sql
@@ -0,0 +1,4 @@
+INSERT INTO `ngms`.`dict` (`ID`, `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ('348', 'ODORS_METHOD', '', '加臭器充装方式组', 'G', NULL, '1', '加臭器管理模块充装方式组');
+INSERT INTO `ngms`.`dict` (`ID`, `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ('349', 'ODORS_METHOD', 'repl', '药罐替换', 'I', '1', '1', '');
+INSERT INTO `ngms`.`dict` (`ID`, `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ('350', 'ODORS_METHOD', 'fill', '余量充装', 'I', '2', '1', '');
+INSERT INTO `ngms`.`dict` (`ID`, `GROUP_CODE`, `CODE`, `NAME`, `TYPE`, `THESORT`, `STATE`, `REMARKS`) VALUES ('351', 'ODORS_METHOD', 'cfm', '余量确认', 'I', '3', '1', '');
